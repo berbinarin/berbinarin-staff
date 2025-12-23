0d70c358a9486525571896e39875db6f4376e0bb
Devsam_ <sabillyamri1@gmail.com>
2025-12-23 13:03:48 +0700

    feat : add layout login

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    // public function register(UserRequest $request)
+    // {
+    //     try {
+    //         $validated = $request->validated();
+
+    //         User::create([
+    //             'username' => $validated["username"],
+    //             'email' => $validated['email'],
+    //             'password' => bcrypt($validated['password']),
+    //             "role" => $validated["role"]
+    //         ]);
+
+
+    //          return redirect()->back()->with([
+    //             'alert' => true,
+    //             'type' => 'success',
+    //             'title' => 'Berhasil!',
+    //             'message' =>'Pendaftaran Berhasil',
+    //             'icon' => asset('assets/images/dashboard/success.webp'),
+    //         ]);
+    //     } catch (\Exception $e) {
+    //         return redirect()->back()->with([
+    //             'alert' => true,
+    //             'type' => 'error',
+    //             'title' => 'Gagal!',
+    //             'message' =>'Terjadi kesalahan, silahkan coba lagi',
+    //             'icon' => asset('assets/images/dashboard/error.webp'),
+    //         ]);
+    //     }
+    // }
+
+    // public function login()
+    // {
+    //      $agent = new Agent();
+
+    //     if ($agent->isMobile() && !$agent->isTablet()) {
+    //         return view('auth.block-mobile');
+    //     }
+    //     return view('auth.login');
+    // }
+
+    public function authenticated(Request $request)
+    {
+        $credentials = $request->only('username', 'password');
+
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('dashboard.index')->with([
+                'alert' => true,
+                'type' => 'success',
+                'title' => 'Login Berhasil',
+                'message' => 'Silahkan masuk',
+                'icon' => asset('assets/images/dashboard/success.webp'),
+            ]);
+        } else {
+            return redirect()->route('home.index')->with([
+                'alert' => true,
+                'type' => 'error',
+                'title' => 'Gagal!',
+                'message' => 'Username atau Password salah',
+                'icon' => asset('assets/images/dashboard/error.webp'),
+            ]);
+        }
+    }
+
+    // public function Logout()
+    // {
+    //     Auth::logout();
+    //     return redirect()->route('auth.login')->with([
+    //             'alert' => true,
+    //             'type' => 'success',
+    //             'title' => 'Logout Berhasil',
+    //             'message' =>'Sampai jumpa lagi 😘',
+    //             'icon' => asset('assets/images/dashboard/success.webp'),
+    //         ]);
+    // }
+
+    public function index()
+    {
+        return view('auth.login');
+    }
+}
---
+++ routes/auth.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\Auth\AuthController;
+use Illuminate\Support\Facades\Route;
+
+// Route::post('/register', [AuthController::class, 'register'])->name('auth.register')->middleware('guest');
+// Route::get('/login', [AuthController::class, 'login'])->name('auth.login')->middleware('guest');
+// Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout')->middleware('auth');
+
+Route::post('/login', [AuthController::class, 'authenticated'])->name('auth.authenticated')->middleware('guest');
+Route::get('/login', [AuthController::class, 'index'])->name('home.index');
