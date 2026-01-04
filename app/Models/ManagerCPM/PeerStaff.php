22d55a8697f8e9c3629f54e14bbbb0e7f54923c0
citrahlzh <citrahalijah1992@gmail.com>
2026-01-04 21:19:12 +0700

    feat: add softdeletes to other tables

--- app/Models/ManagerCPM/PeerStaff.php
+++ app/Models/ManagerCPM/PeerStaff.php
@@ -4,10 +4,11 @@ namespace App\Models\ManagerCPM;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PeerStaff extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $table = 'peerstaff';
 
